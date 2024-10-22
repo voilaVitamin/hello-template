45c2b43039180659c57112e9f328a7b34982af8b
Voila Vitamin <148288760+voilaVitamin@users.noreply.github.com>
2024-10-22 02:31:38 -0400

    feat: initial clone of gtech

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import CenteredText from "./components/CenteredText";
+
+function App() {
+  return (
+    <div className="App">
+      <CenteredText />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CenteredText.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './CenteredText.css';
+const CenteredText = () => {
+  return (
+    <div className="centered-text">
+      <p>HELLO TEMPLATE</p>
+      
+    </div>
+  );
+};
+export default CenteredText;
